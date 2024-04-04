ce33dab41f37039a056fc939dd7f75ad303d2bb3
jrama <jramanauskas120@gmail.com>
2024-04-04 16:06:17 +0200

    + Hashset For CropStorage created + Adding crops created. + coding convention

--- src/com/farm/farm/CropStorage.java
+++ src/com/farm/farm/CropStorage.java
@@ -1,7 +1,17 @@
 package com.farm.farm;
 
+import com.farm.crop.Crop;
 
+import java.util.HashSet;
 
 public class CropStorage {
+    static HashSet<Crop> crops = new HashSet<>();
 
+    public static HashSet<Crop> getCrops() {
+        return crops;
+    }
+
+    public static void addCrop(Crop crop) {
+        crops.add(crop);
+    }
 }
