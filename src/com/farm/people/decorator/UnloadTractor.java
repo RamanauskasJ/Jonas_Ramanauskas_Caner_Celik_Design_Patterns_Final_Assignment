6aecc46925930723d40db0e60254584fcfbcdde5
skitzke <egslotz@gmail.com>
2024-04-05 15:07:56 +0200

    + Linked UnloadTractor extending WorkTypeDecorator + Added necessary super

--- src/com/farm/people/decorator/UnloadTractor.java
+++ src/com/farm/people/decorator/UnloadTractor.java
@@ -1,6 +1,23 @@
 package com.farm.people.decorator;
 
+import com.farm.farm.Farm;
+import com.farm.people.worker.Worker;
 
-public class UnloadTractor  {
+public class UnloadTractor extends WorkTypeDecorator{
+    public UnloadTractor(Worker worker) {
+        super(worker);
+    }
 
+    @Override
+    public void work(Farm farm) {
+        worker.work(farm);
+        if (farm.getTractor().isAtCropStorage()) {
+            farm.getTractor().unloadTractor();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Tractor unLoader";
+    }
 }
