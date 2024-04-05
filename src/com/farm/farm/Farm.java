bdb30615b326c3301b0a2e7d7a593e21db1cc25c
skitzke <egslotz@gmail.com>
2024-04-05 12:01:49 +0200

    + Added super for TractorDriver from Worker + Added Farm in order for other classes to use certain methods. + Added HarvestCrop method + Added removing of over worked workers method to Farm

--- src/com/farm/farm/Farm.java
+++ src/com/farm/farm/Farm.java
@@ -1,6 +1,63 @@
 package com.farm.farm;
 
+import com.farm.crop.Crop;
+import com.farm.people.Manager;
+import com.farm.people.worker.Worker;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Queue;
 
 public abstract class Farm {
+    protected Queue<Crop> crops = new ArrayDeque<>();
+    private final ArrayList<Worker> workers = new ArrayList<>();
+    private final Queue<Crop> unloadedCrops = new ArrayDeque<>();
+    private final Tractor tractor = new Tractor();
+    private final int maxWorkers;
 
+    public Farm(int maxWorkers) {
+        Manager.GetManager().addFarm(this);
+        this.maxWorkers = maxWorkers;
+    }
+
+    public ArrayList<Worker> getWorkers() {
+        return workers;
+    }
+
+    public void addWorker(Worker worker) {
+        if (this.workers.size() < maxWorkers) {
+            this.workers.add(worker);
+        }
+    }
+
+    public void harvestCrops() {
+        Crop nextCrop = getNextCrop();
+
+        if (nextCrop != null) {
+            this.crops.remove(nextCrop);
+            this.unloadedCrops.add(nextCrop);
+        }
+    }
+
+    private Crop getNextCrop() {
+        return this.crops.peek();
+    }
+
+    public Queue<Crop> getUnloadedCrops() {
+        return unloadedCrops;
+    }
+
+    public Tractor getTractor() {
+        return tractor;
+    }
+
+    public abstract Crop GenerateCrop();
+
+    public void removeOverWorkedWorkers() {
+
+        workers.removeIf(Worker::isWorkerOverWorked);
+    }
+
+    public void fireWorker(Worker worker) {
+        workers.remove(worker);
+    }
 }
--- src/com/farm/people/worker/TractorDriver.java
+++ src/com/farm/people/worker/TractorDriver.java
@@ -1,6 +1,20 @@
 package com.farm.people.worker;
 
+import com.farm.farm.Farm;
 
-public class TractorDriver {
+public class TractorDriver extends Worker{
+    @Override
+    public void work(Farm farm) {
+        if (!farm.getTractor().isAtCropStorage() && farm.getTractor().isFull()) {
+            farm.getTractor().setAtCropStorage(true);
+        } else if (farm.getTractor().isAtCropStorage() && !farm.getTractor().isFull()) {
+            farm.getTractor().setAtCropStorage(false);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "tractor driver";
+    }
 
 }
