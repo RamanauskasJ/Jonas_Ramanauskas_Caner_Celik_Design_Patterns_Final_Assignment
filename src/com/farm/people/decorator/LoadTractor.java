901aef240a39585622a3825b86ee56686b66b256
skitzke <egslotz@gmail.com>
2024-04-05 13:30:37 +0200

    + Created Farmer that extends from Worker + Linked LoadTractor extending WorkTypeDecorator + Added if check for when crop storage is full in LoadTractor

--- src/com/farm/people/decorator/LoadTractor.java
+++ src/com/farm/people/decorator/LoadTractor.java
@@ -1,6 +1,34 @@
 package com.farm.people.decorator;
 
+import com.farm.crop.Crop;
+import com.farm.farm.Farm;
+import com.farm.people.worker.Worker;
 
-public class LoadTractor  {
+public class LoadTractor extends WorkTypeDecorator{
+
+    public LoadTractor(Worker worker) {
+        super(worker);
+    }
+
+    public void work(Farm farm) {
+        worker.work(farm);
+        Crop nextCrop = farm.getUnloadedCrops().peek();
+
+        worker.decreaseHungerMeter(10);
+        worker.decreaseSanityMeter(20);
+        worker.increaseTimesWorked();
+
+        if (!farm.getTractor().isAtCropStorage()) {
+            if (!farm.getTractor().isFull()) {
+                farm.getTractor().loadTractor(nextCrop);
+                farm.getUnloadedCrops().remove(nextCrop);
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Tractor loader";
+    }
 
 }
--- src/com/farm/people/worker/Farmer.java
+++ src/com/farm/people/worker/Farmer.java
@@ -1,8 +1,19 @@
 package com.farm.people.worker;
 
+import com.farm.farm.Farm;
 
+public class Farmer extends Worker{
 
-public class Farmer  {
+    public Farmer() {
+    }
 
+    @Override
+    public void work(Farm farm) {
+        farm.harvestCrops();
+    }
 
+    @Override
+    public String toString() {
+        return "farmer";
+    }
 }
