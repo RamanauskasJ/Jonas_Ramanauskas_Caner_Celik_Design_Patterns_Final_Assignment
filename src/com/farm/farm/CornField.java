3d71ba629df7dc9466034f2bea5d5759a5329f91
jrama <jramanauskas120@gmail.com>
2024-04-05 12:38:21 +0200

    + Abstract Method for GeneratingCrop + Max_Workers Limit

--- src/com/farm/farm/CornField.java
+++ src/com/farm/farm/CornField.java
@@ -1,6 +1,32 @@
 package com.farm.farm;
 
+import com.farm.crop.CornCrop;
+import com.farm.crop.Crop;
+import com.farm.crop.OatCrop;
+import com.farm.crop.WheatCrop;
+import java.util.Random;
 
-public class CornField  {
+public class CornField extends Farm {
+    private static final int MAX_WORKERS = 15;
 
+    public CornField() {
+        super(MAX_WORKERS);
+        for (int i = 0; i < 1000; i++) {
+            super.crops.add(GenerateCrop());
+        }
+    }
+
+    @Override
+    public Crop GenerateCrop() {
+        Random random = new Random();
+        int rand = random.nextInt(6); // Generates a random number between 0 (inclusive) and 6 (exclusive)
+        double kg = random.nextDouble() * (1000 - 20) + 20; // Generates a random weight between 20 and 1000 kg
+
+        return switch (rand) {
+            case 0, 1, 2 -> new CornCrop(kg);
+            case 3, 4 -> new WheatCrop(kg);
+            case 5 -> new OatCrop(kg);
+            default -> throw new RuntimeException("Defaulted exception");
+        };
+    }
 }
