5d4468caee12799b2db465d9159721107f8f24e8
skitzke <egslotz@gmail.com>
2024-04-06 12:55:16 +0200

    + Coding Conventions + Created createFarm method + Created options for certain fields

--- src/com/farm/App.java
+++ src/com/farm/App.java
@@ -1,9 +1,13 @@
 package com.farm;
 
+import com.farm.farm.CornField;
 import com.farm.farm.Farm;
+import com.farm.farm.OatField;
+import com.farm.farm.WheatField;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.io.IOException;
 
 public class App {
     private static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
@@ -23,5 +27,37 @@ public class App {
                             3. Observe crop storage.
                         """
         );
+    }
+
+    public void createFarm(Farm farm) {
+        this.farms.add(farm);
+        home();
+    }
+
+    public void createFarms() {
+        System.out.println(
+                """
+                            What kind of farm would you like to create?
+                            1. Corn Field
+                            2. Oat Field
+                            3. Wheat Field
+                            
+                            Press `q` to cancel
+                        """
+        );
+
+        try {
+            String choice = reader.readLine();
+
+            switch (choice) {
+                case "1" -> createFarm(new CornField());
+                case "2" -> createFarm(new OatField());
+                case "3" -> createFarm(new WheatField());
+                case "q" -> home();
+                default -> createFarms();
+            }
+        } catch (IOException e) {
+            createFarms();
+        }
     }
 }
