728e8231e880952390a24a63463a327f70803b8e
skitzke <egslotz@gmail.com>
2024-04-06 11:30:51 +0200

    + Added Main running App when running Main + Initial logic to terminal prompts added with BufferedReader import

--- src/com/farm/App.java
+++ src/com/farm/App.java
@@ -1,6 +1,27 @@
 package com.farm;
 
+import com.farm.farm.Farm;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class App {
+    private static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
+    private final ArrayList<Farm> farms;
+
+    public App(ArrayList<Farm> farms) {
+        this.farms = farms;
+    }
+
+    public void home() {
+        System.out.println(
+                """
+                           Greetings Farm Manager. What would you like to do today?
+                            1. Create a new farm.
+                            2. Manage an existing farm.
+                            3. Observe crop storage.
+                        """
+        );
+    }
 }
--- src/com/farm/Main.java
+++ src/com/farm/Main.java
@@ -1,8 +1,12 @@
 package com.farm;
 
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+        App app = new App(new ArrayList<>());
 
+        app.home();
     }
 }
